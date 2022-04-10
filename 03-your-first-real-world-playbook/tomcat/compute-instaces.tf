e81dc2e68bda3baf91baf1ff0cfe03c4b6eb8f45
tajpouria <tajpouria.dev@gmail.com>
2022-04-10 10:34:50 +0400

    create 8 files

---
+++ 03-your-first-real-world-playbook/tomcat/compute-instaces.tf
@@ -0,0 +1,98 @@
+variable "project" {
+  type = string
+}
+
+terraform {
+  required_providers {
+    tls = {
+      source  = "hashicorp/tls"
+      version = "3.1.0"
+    }
+    google = {
+      source  = "hashicorp/google"
+      version = "4.5.0"
+    }
+  }
+}
+
+provider "tls" {
+  // no config needed
+}
+
+resource "tls_private_key" "ssh" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "local_file" "ssh_private_key_pem" {
+  content         = tls_private_key.ssh.private_key_pem
+  filename        = ".ssh/google_compute_engine"
+  file_permission = "0600"
+}
+
+provider "google" {
+  project = var.project
+  region  = "europe-west1"
+  zone    = "europe-west1-b"
+}
+
+resource "google_project_service" "cloud_resource_manager" {
+  service            = "cloudresourcemanager.googleapis.com"
+  disable_on_destroy = false
+}
+
+resource "google_project_service" "compute" {
+  service            = "compute.googleapis.com"
+  disable_on_destroy = false
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "ubuntu-vpc"
+}
+
+resource "google_compute_address" "static_ip" {
+  name = "ubuntu-vm"
+}
+
+resource "google_compute_firewall" "web_server" {
+  name          = "web-server"
+  network       = google_compute_network.vpc_network.name
+  target_tags   = ["web-server"] // this targets our tagged VM
+  source_ranges = ["0.0.0.0/0"]
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22", "80", "443"]
+  }
+}
+
+data "google_client_openid_userinfo" "me" {}
+
+resource "google_compute_instance" "ubuntu_vm" {
+  name         = "ubuntu"
+  machine_type = "f1-micro"
+  tags         = ["web-server"] // this receives the firewall rule
+
+  metadata = {
+    ssh-keys = "${split("@", data.google_client_openid_userinfo.me.email)[0]}:${tls_private_key.ssh.public_key_openssh}"
+  }
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-1804-lts"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+
+    access_config {
+      nat_ip = google_compute_address.static_ip.address
+    }
+  }
+}
+
+output "ubuntu_public_ip" {
+  value = google_compute_address.static_ip.address
+}
+
